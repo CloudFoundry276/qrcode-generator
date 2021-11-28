b352cb4de7287b37095dc083e0ab2fd80062b2df
CloudFoundry276 <cloudfoundry276@gmail.com>
2021-11-28 15:30:32 +0530

    done

---
+++ qrcode_generator.py
@@ -0,0 +1,17 @@
+import qrcode
+import image
+
+
+qr = qrcode.QRCode(
+    version = 5,
+    box_size = 10,
+    border = 5
+)
+
+#data = "https://www.youtube.com/c/TechieCoder/playlists"
+data = "Sagar Pitalekar"
+
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill="black", back_color="white")
+img.save("test2.png")
